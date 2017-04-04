a72900d531abf996c655d69d3c5e935e194208a0
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-03 17:01:36 -0700

    Initial Commit

---
+++ SVNStandardButton/SVNStandardButton.h
@@ -0,0 +1,19 @@
+//
+//  SVNStandardButton.h
+//  SVNStandardButton
+//
+//  Created by Aaron Dean Bikis on 4/3/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNStandardButton.
+FOUNDATION_EXPORT double SVNStandardButtonVersionNumber;
+
+//! Project version string for SVNStandardButton.
+FOUNDATION_EXPORT const unsigned char SVNStandardButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNStandardButton/PublicHeader.h>
+
+
